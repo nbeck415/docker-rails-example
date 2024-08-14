23130a1a380c74eabb9c3259a7081a68fb4aea56
Natalie <natalielunbeck@yahoo.com>
2024-08-14 13:20:48 -0700

    inject host

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.hosts << *.shipyard.host
+config.hosts << "46b6e312-8004-40d9-8f2b-4b49280a7d3a.dev.nbeck415.shipyard.host"
